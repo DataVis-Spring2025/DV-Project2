5359d58cb7ca58796c00b95f11766a13e3105948
Rashi455 <rashiloni@gmail.com>
2025-03-14 14:24:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ d3.csv("data/2024-2025.csv")
       //convert from string to number
       d.latitude = +d.latitude;
       d.longitude = +d.longitude;
+      d.mag = +d.mag;
     });
 
     // Initialize chart and then show it
     const leafletMap = new LeafletMap({ parentElement: "#my-map" }, data);
+    lineChart = new MagnitudeChart({ parentElement: "#magnitudeChart" }, data);
 
     // Initialize timeline with leafletMap
     const timeline = new Timeline(leafletMap);
